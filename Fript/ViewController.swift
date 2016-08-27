947a78642d2517452fa76ab923818704e53898ce
oyvodega <oyvind.odegard@finn.no>
2016-08-27 22:28:58 +0200

    Initial Commit

---
+++ Fript/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fript
+//
+//  Created by Ødegård, Øyvind on 27.08.2016.
+//  Copyright © 2016 Ødegård, Øyvind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
