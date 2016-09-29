10b213064c8d024dbe5cf1d24cd9531979a79e8a
oyvodega <oyvind.odegard@finn.no>
2016-09-29 15:01:00 +0200

    Tmp commit.

--- Fript/ViewController.swift
+++ Fript/ViewController.swift
@@ -13,6 +13,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if (FBSDKAccessToken.currentAccessToken() != nil)
+        {
+            // User is already logged in, do work such as go to next view controller.
+        }
+        else
+        {
+            let loginView : FBSDKLoginButton = FBSDKLoginButton()
+            self.view.addSubview(loginView)
+            loginView.center = self.view.center
+            loginView.readPermissions = ["public_profile", "email", "user_friends"]
+            //loginView.delegate = self
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
